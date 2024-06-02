17f10cc8c6451bd338727a12edba5544c6017436
Kacper Borucki <63121296+borutzki@users.noreply.github.com>
2024-06-02 12:06:01 +0200

    Apply new classes to keywords

--- ArchiveLibrary/keywords.py
+++ ArchiveLibrary/keywords.py
@@ -1,94 +1,93 @@
 #!/usr/bin/env python
 
-import os
-import tarfile
 import zipfile
 
 from robot.libraries.Collections import Collections
 from robot.libraries.OperatingSystem import OperatingSystem
 
-from .utils import Unzip, Untar, return_files_lists
+from .utils import Zip, Tar, TarGz
+from pathlib import Path
 
 
 class ArchiveKeywords:
-    ROBOT_LIBRARY_SCOPE = 'Global'
+    ROBOT_LIBRARY_SCOPE = "Global"
 
     compressions = {
         "stored": zipfile.ZIP_STORED,
         "deflated": zipfile.ZIP_DEFLATED,
         "bzip2": zipfile.ZIP_BZIP2,
-        "lzma": zipfile.ZIP_LZMA
+        "lzma": zipfile.ZIP_LZMA,
     }
 
-    tars = ['.tar', '.tar.bz2', '.tar.gz', '.tgz', '.tz2']
+    tars = [".tar", ".tar.bz2", ".tar.gz", ".tgz", ".tz2"]
 
-    zips = ['.docx', '.egg', '.jar', '.odg', '.odp', '.ods', '.xlsx', '.odt',
-            '.pptx', 'zip']
+    zips = [
+        ".docx",
+        ".egg",
+        ".jar",
+        ".odg",
+        ".odp",
+        ".ods",
+        ".xlsx",
+        ".odt",
+        ".pptx",
+        ".zip",
+    ]
 
     def __init__(self):
         self.oslib = OperatingSystem()
         self.collections = Collections()
 
-    def extract_zip_file(self, zip_file, dest=None):
-        """ Extract a ZIP file
+    def extract_zip_file(self, zip_file: Path, dest: Path | None = None) -> None:
+        """Extract a ZIP file
 
         `zip_file` the path to the ZIP file
 
         `dest` optional destination folder. Assumes current working directory if it is none
                It will be created if It doesn't exist.
         """
+        if dest is None:
+            dest = Path(".")
+        zip = Zip(zip_file)
+        zip.extract(dest)
 
-        if dest:
-            self.oslib.create_directory(dest)
-            self.oslib.directory_should_exist(dest)
-        else:
-            dest = os.getcwd()
-
-        cwd = os.getcwd()
-
-        # Dont know why I a have `gotta catch em all` exception handler here
-        try:
-            Unzip().extract(zip_file, dest)
-        except:
-            raise
-        finally:
-            os.chdir(cwd)
-
-    def extract_tar_file(self, tar_file, dest=None):
-        """ Extract a TAR file
+    def extract_tar_file(self, tar_file: Path, dest: Path | None = None) -> None:
+        """Extract a TAR file
 
         `tar_file` the path to the TAR file
 
         `dest` optional destination folder. Assumes current working directory if it is none
                It will be created if It doesn't exist.
         """
-        if dest:
-            self.oslib.create_directory(dest)
-        else:
-            dest = os.getcwd()
+        if dest is None:
+            dest = Path(".")
+        tar = Tar(tar_file)
+        tar.extract(dest)
 
-        self.oslib.file_should_exist(tar_file)
-
-        Untar().extract(tar_file, dest)
-
-    def archive_should_contain_file(self, zip_file, filename):
-        """ Check if a file exists in the ZIP file without extracting it
+    def archive_should_contain_file(self, zip_file: Path, filename: str) -> None:
+        """Check if a file exists in the ZIP file without extracting it
 
         `zip_file` the path to the ZIP file
 
         `filename` name of the file to search for in `zip_file`
         """
-        self.oslib.file_should_exist(zip_file)
+        if zip_file.suffix in self.zips:
+            list = Zip(zip_file).list_content()
+        elif zip_file.suffix in self.tars:
+            list = Tar(zip_file).list_content()
+        else:
+            raise NameError("Incorrect archive type!")
 
-        files = zipfile.ZipFile(zip_file).namelist() if zipfile.is_zipfile(zip_file) else tarfile.open(
-            zip_file).getnames()
+        assert filename in list, f"File {filename} not found in archive {zip_file}!"
 
-        files = [os.path.normpath(item) for item in files]
-
-        self.collections.list_should_contain_value(files, filename)
-
-    def create_tar_from_files_in_directory(self, directory, filename, sub_directories=True, tgz=False):
-        """ Take all files in a directory and create a tar package from them
+    def create_tar_from_files_in_directory(
+        self,
+        directory: Path,
+        filename: Path,
+        sub_directories: bool = True,
+        tgz: bool = False,
+    ):
+        """Take all files in a directory and create a tar package from them
 
         `directory` Path to the directory that holds our files
 
@@ -98,20 +97,21 @@ class ArchiveKeywords:
 
         `tgz` Creates a .tgz / .tar.gz archive (compressed tar package) instead of a regular tar - False by default.
         """
+        # TODO: Restore option to include / exclude subdirectories
         if tgz:
-            tar = tarfile.open(filename, "w:gz")
+            TarGz.archive(directory, filename)
         else:
-            tar = tarfile.open(filename, "w")
-
-        files = return_files_lists(directory, sub_directories)
-        for filepath, name in files:
-            tar.add(filepath, arcname=name)
-
-        tar.close()
+            Tar.archive(directory, filename)
 
     @classmethod
-    def create_zip_from_files_in_directory(cls, directory, filename, sub_directories=False, compression="stored"):
-        """ Take all files in a directory and create a zip package from them
+    def create_zip_from_files_in_directory(
+        cls,
+        directory: Path,
+        filename: Path,
+        sub_directories: bool = False,
+        compression: str = "stored",
+    ):
+        """Take all files in a directory and create a zip package from them
 
         `directory` Path to the directory that holds our files
 
@@ -121,20 +121,6 @@ class ArchiveKeywords:
 
         `compression` stored (default; no compression), deflated, bzip2 (with python >= 3.3), lzma (with python >= 3.3)
         """
-
-        if cls.compressions.get(compression) is None:
-            raise ValueError("Unknown compression method")
-        comp_method = cls.compressions.get(compression)
-
-        the_zip = zipfile.ZipFile(filename, "w", comp_method)
-
-        files = return_files_lists(directory, sub_directories)
-        for filepath, name in files:
-            the_zip.write(filepath, arcname=name)
-
-        the_zip.close()
-
-
-if __name__ == '__main__':
-    al = ArchiveKeywords()
-    al.extract('test.zip')
+        # TODO: Restore option to include / exclude subdirectories
+        # TODO: Restore option to choose compression type
+        Zip.archive(directory, filename)
